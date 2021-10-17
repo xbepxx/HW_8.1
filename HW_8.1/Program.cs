d8538f48979d34d28c003b2f1b140d942da5a430
xbepxx <xbepxx@gmail.com>
2021-10-17 21:58:28 +0300

    Добавьте файлы проекта.

---
+++ HW_8.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HW_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/Name/Desktop/Обучение/Автоматизация BIM/Модуль 1";
+            string [] cat = Directory.GetDirectories(path);
+            foreach (string s in cat)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
